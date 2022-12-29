be156054b0cfee57bd27e7536482ba212bc9a1df
Petz <gabriel.santana@jointecnologia.com.br>
2022-12-29 00:08:33 -0300

    feat: Add more types of formatters to formatters.js.
    Signed-off-by: Petz <gabriel.santana@jointecnologia.com.br>

--- src/services/formatters.js
+++ src/services/formatters.js
@@ -4,17 +4,45 @@ const DateTimeFormatter = new Intl.DateTimeFormat(window.navigator.language, {
     timeZone: 'America/Sao_Paulo'
 });
 
-const NumberFormatter = new Intl.NumberFormat(window.navigator.language, {
+const CurrencyFormatter = new Intl.NumberFormat(window.navigator.language, {
     style: 'currency',
     currency: 'BRL',
     useGrouping: true,
 });
 
-export function dateTimeFormat(date) {
-    return DateTimeFormatter.format(date);
+const DecimalFormatter = new Intl.NumberFormat(window.navigator.language, {
+    style: 'decimal',
+    useGrouping: true,
+});
+
+const IntegerFormatter = new Intl.NumberFormat(window.navigator.language, {
+    style: 'decimal',
+    useGrouping: true,
+    maximumFractionDigits: 0,
+});
+
+const PercentageFormatter = new Intl.NumberFormat(window.navigator.language, {
+    style: 'decimal',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
+
+class Formatters {
+    static dateTimeFormat(date) {
+        return DateTimeFormatter.format(date);
+    }
+    static currencyFormat(value) {        
+        return CurrencyFormatter.format(value);
+    }
+    static decimalFormat(value) {
+        return DecimalFormatter.format(value);
+    }    
+    static integerFormat(value) {
+        return IntegerFormatter.format(value);
+    }
+    static percentageFormat(value) {
+        return PercentageFormatter.format(value);
+    }
 }
 
-export function currencyFormat(value) {
-    return NumberFormatter.format(value);
-}
-// export function NumberFormatter.format;
+export default Formatters;
